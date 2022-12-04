69ec8415644e0d516d42014a276793ab725e442a
Ziflie <rayanmeziani95@outlook.com>
2022-12-04 01:15:24 +0100

    initial commit

--- pages/movies.js
+++ pages/movies.js
@@ -1,24 +1,5 @@
 import clientPromise from "../lib/mongodb";
 
-export default function Movies({ movies }) {
-    return (
-        <div>
-            <h1>Top 20 Movies of All Time</h1>
-            <p>
-                <small>(According to Metacritic)</small>
-            </p>
-            <ul>
-                {movies.map((movie) => (
-                    <li>
-                        <h2>{movie.title}</h2>
-                        <h3>{movie.metacritic}</h3>
-                        <p>{movie.plot}</p>
-                    </li>
-                ))}
-            </ul>
-        </div>
-    );
-}
 
 export async function getServerSideProps() {
     try {
@@ -39,3 +20,25 @@ export async function getServerSideProps() {
         console.error(e);
     }
 }
+
+export default function Movies({ movies }) {
+    return (
+        <div>
+            <h1>Top 20 Movies of All Time</h1>
+            <p>
+                <small>(According to Metacritic)</small>
+            </p>
+            <ul>
+                {movies.map((movie) => (
+                    <li>
+                        <h2>{movie.title}</h2>
+                        <h3>{movie.metacritic}</h3>
+                        <p>{movie.plot}</p>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
+}
+
+ 
--- pages/top.js
+++ pages/top.js
@@ -1,24 +1,5 @@
 import clientPromise from "../lib/mongodb";
 
-export default function Top({ movies }) {
-  return (
-    <div>
-      <h1>Top 1000 Movies of All Time</h1>
-      <p>
-        <small>(According to Metacritic)</small>
-      </p>
-      <ul>
-        {movies.map((movie) => (
-          <li>
-            <h2>{movie.title}</h2>
-            <h3>{movie.metacritic}</h3>
-            <p>{movie.plot}</p>
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
-}
 
 export async function getStaticProps() {
     try {
@@ -40,3 +21,26 @@ export async function getStaticProps() {
     }
 }
         
+
+
+
+export default function Top({ movies }) {
+  return (
+    <div>
+      <h1>Top 1000 Movies of All Time</h1>
+      <p>
+        <small>(According to Metacritic)</small>
+      </p>
+      <ul>
+        {movies.map((movie) => (
+          <li>
+            <h2>{movie.title}</h2>
+            <h3>{movie.metacritic}</h3>
+            <p>{movie.plot}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+ 
